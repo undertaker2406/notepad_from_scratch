4c697cccc7d603a6f8357fdaeebafa695efc083d
undertaker2406 <vmc240606@gmail.com>
2026-02-21 00:04:28 +0700

    gio se den Tildes (~)

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 /*** includes ***/
+
 #include<unistd.h>
 #include<termios.h>
 #include<stdlib.h>
@@ -7,13 +8,20 @@
 #include<errno.h>
 
 /*** defines ***/
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
+
 struct termios orig_termios;
 
 /*** terminal ***/
+
 void die(const char *s){
+    //clear the screen
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    
     perror(s);
     exit(1);
 }
@@ -39,19 +47,46 @@ void enableRawMode(){
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey(){
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1){
+        if(nread == -1 && errno != EAGAIN)  die("read");
+    }
+    return c;
+}
+
+/*** output ***/
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** input ***/
+
+void editorProcessKeyPress(){
+    char c = editorReadKey();
+
+    switch(c){
+        case CTRL_KEY('q'):
+            //clear the screen
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            
+            exit(0);
+            break;
+    }
+}
+
 /*** init ***/
+
 int main(){
     enableRawMode();
     
     while(1){
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if(iscntrl(c)){
-            printf("%d\r\n" ,c);
-        } else {
-            printf("%d('%c')\r\n",c,c);
-        }
-        if(c == CTRL_KEY('q'))    break;
+        editorRefreshScreen();
+        editorProcessKeyPress();
     }
     
     return 0;
